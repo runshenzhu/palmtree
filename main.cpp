d18093c3d948e994459b7d9a97eff7c5e971aa88
Xian Ran <xranthoar@gmail.com>
2016-04-21 19:48:51 -0400

    cant pass interleaved insert/remove

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <assert.h>
 #include "palmtree.h"
 #include <thread>
+#include <cstdlib>
 #include <glog/logging.h>
+#include <map>
 
 using namespace std;
 
@@ -17,8 +19,8 @@ int main(int argc, char *argv[]) {
   palmtree::PalmTree<int, int> *palmtreep = &palmtree;
 
   std::vector<std::thread> threads;
-  for (int i = 0; i < 5; i++) {
-//    threads.push_back(std::thread([palmtreep, i]() {
+  for (int i = 0; i < 16; i++) {
+   // threads.push_back(std::thread([palmtreep, i]() {
       int res;
       palmtreep->insert(i, i);
       bool success = palmtreep->find(i, res);
@@ -30,7 +32,7 @@ int main(int argc, char *argv[]) {
 //    }));
   }
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 16; i++) {
     DLOG(INFO) << "Remove " << i;
     palmtreep->remove(i);
     int res;
@@ -40,6 +42,31 @@ int main(int argc, char *argv[]) {
       assert(false);
     } else {
       DLOG(INFO) << "Thread " << i << " get nothing";
+    }
+  }
+
+  srand(15618);
+
+  std::map<int, int> reference;
+  for (int i = 10; i < 256; i++) {
+    int key1 = i;
+    int value1 = rand() % 10;
+    int key2 = i - 10;
+
+    palmtreep->insert(key1, value1);
+    palmtreep->remove(key2);
+
+    reference.emplace(key1, value1);
+    reference.erase(key2);
+  }
+
+  for (int i = 0; i < 256; i++) {
+    int res;
+    bool suc = palmtreep->find(i, res);
+    if (reference.find(i) != reference.end()) {
+      CHECK(suc == false) << "Should not find anything";
+    } else {
+      CHECK(suc == true && res == reference[i]) << "Should find " << i << " " << reference[i];
     }
   }
 
